952b0f09d357000ff6b54b8a7d8948ce90ab97fc
timdeluxe <5765175+timdeluxe@users.noreply.github.com>
2022-12-06 17:41:32 +0100

    BREAKING CHANGE feat: renaming enforce_private_link_endpoint_network_policies variable and subnet option due to upstream (azurerm provider) naming change

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ resource "azurerm_virtual_network" "virtual-network" {
 resource "azurerm_subnet" "subnet" {
   for_each = var.subnets
 
-  address_prefixes                               = each.value.cidr
-  name                                           = each.key
-  resource_group_name                            = var.resource_group
-  virtual_network_name                           = azurerm_virtual_network.virtual-network.name
-  service_endpoints                              = each.value.service_endpoints
-  enforce_private_link_endpoint_network_policies = each.value.enforce_private_link_endpoint_network_policies
+  address_prefixes                          = each.value.cidr
+  name                                      = each.key
+  resource_group_name                       = var.resource_group
+  virtual_network_name                      = azurerm_virtual_network.virtual-network.name
+  service_endpoints                         = each.value.service_endpoints
+  private_endpoint_network_policies_enabled = each.value.private_endpoint_network_policies_enabled
 }
